0d0642b7625bca801aac209c97b209065393ed91
lukemm23 <lukemm23@gmail.com>
2020-01-09 11:15:06 -0600

    environment variable created

--- database.sql
+++ database.sql
@@ -28,3 +28,37 @@ VALUES ('Admirals Only: Captain Borodin is totally weird.', 13),
 ('Captains Or Above: Lieutenant Ryan is looking fly.', 6),
 ('Lieutenants Or Above: We are heading to the Bahamas.', 3);
 
+-- hashing update
+
+DROP TABLE "user";
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "clearance_level" INTEGER NOT NULL DEFAULT 0
+);
+
+INSERT INTO "user" ("username", "password", "clearance_level")
+VALUES ('Admiral Greer', '$2b$10$p5Wkte33hlOBOcUtJie6H.PnCvk8v.KjZspVoAFtT7g5v5xK.EXVG', 18),
+('Captain Borodin', '$2b$10$p5Wkte33hlOBOcUtJie6H.ZIgFjzr4zY8FItxC8gZyqIWD5gYmL0m', 10),
+('Lieutenant Nguyen', '$2b$10$p5Wkte33hlOBOcUtJie6H.vaUd5ikB1LWCbVZAA87BR63NiDorn1C', 4),
+('Lieutenant Ryan', '$2b$10$p5Wkte33hlOBOcUtJie6H.PnCvk8v.KjZspVoAFtT7g5v5xK.EXVG', 4);
+
+
+-- salting update
+
+DROP TABLE "user";
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "clearance_level" INTEGER NOT NULL DEFAULT 0
+);
+
+INSERT INTO "user" ("username", "password", "clearance_level")
+VALUES ('Admiral Greer', '$2b$10$uxPm0qeJAz70oqhEg8dX6uXlYc2PWUtPuZhTa65OiDv2LCHA41OLq', 18),
+('Captain Borodin', '$2b$10$iUCrWSMvLpYuKQLsmmTiNe3gfU6jAdyElCbCLtboVH6DlXJdsuPxG', 10),
+('Lieutenant Nguyen', '$2b$10$/3yhbbjXPPf3L4Z1gXDA5OJzJkf6b.2CuvIA8OzP6c8jPEQlbo5re', 4),
+('Lieutenant Ryan', '$2b$10$hr1Tlo6K.yxAq3FC4iIHsuYQwYpjQC8SyDnYykMu/LNB9TXMkxMt2', 4);
